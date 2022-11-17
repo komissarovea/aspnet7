dcf0c372cec320f22415906560c3ef987383bdb4
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-18 00:32:18 +0300

    WebApplication members

--- helloapp/Program.cs
+++ helloapp/Program.cs
@@ -8,7 +8,24 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(options);
 //builder.WebHost
 
 WebApplication app = builder.Build();
+IHost host = app;
+IApplicationBuilder applicationBuilder = app;
+IEndpointRouteBuilder endpointRouteBuilder = app;
+//app.Configuration
+//app.Environment
+//app.Lifetime
+//app.Logger
+//app.Services
+//app.Urls
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+//app.Run();
+//app.RunAsync();
+//app.Start();
+//app.StartAsync();
+//app.StopAsync();
+
+await app.StartAsync();
+await Task.Delay(10000);
+await app.StopAsync();  // через 10 секунд завершаем выполнение приложения
