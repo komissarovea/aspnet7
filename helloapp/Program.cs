beed68f113f42e24a86bb09874ef01964b5637e8
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-18 01:53:33 +0300

    HttpResponse members

--- helloapp/Program.cs
+++ helloapp/Program.cs
@@ -1,35 +1,52 @@
-using Microsoft.AspNetCore.Localization;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-app.MapGet("/",
-    (HttpContext context) =>
-    {
-        return "Hello World!";
-    });
-
-int x = 2;
-int y = 1;
 app.Run(async (context) =>
 {
-    x = x * 2;  //  2 * 2 = 4
-    y++;
-    await context.Response.WriteAsync($"Result: 2^{y} = {x}");
+    HttpResponse response = context.Response;
+    Console.WriteLine(response.Body);
+    Console.WriteLine(response.BodyWriter);
+    Console.WriteLine(response.ContentLength);
+    Console.WriteLine(response.ContentType);
+    Console.WriteLine(response.Cookies);
+    Console.WriteLine(response.HasStarted);
+    Console.WriteLine(response.Headers);
+    Console.WriteLine(response.Headers.Host);
+    Console.WriteLine(response.HttpContext);
+    Console.WriteLine(response.StatusCode);
+
+    response.Headers.ContentLanguage = "ru-RU";
+    response.Headers.ContentType = "text/plain; charset=utf-8";  // response.ContentType
+    response.ContentType = "text/html; charset=utf-8";
+    response.Headers.Append("secret-id", "256");    // добавление кастомного заголовка
+    response.StatusCode = 404;
+    await response.WriteAsync("Привет METANIT.COM", System.Text.Encoding.Default);
+    await response.WriteAsync("<h2>Hello METANIT.COM</h2><h3>Welcome to ASP.NET Core</h3>");
+    //response.SendFileAsync()
 });
-
-app.Run(HandleRequst);
-
-app.Run(async (context) => await context.Response.WriteAsync("Hello METANIT.COM"));
-
-app.UseWelcomePage();
 
 app.Run();
 
-async Task HandleRequst(HttpContext context)
-{
-    await context.Response.WriteAsync("Hello METANIT.COM 2");
-}
+//int x = 2;
+//int y = 1;
+//app.Run(async (context) =>
+//{
+//    x = x * 2;  //  2 * 2 = 4
+//    y++;
+//    await context.Response.WriteAsync($"Result: 2^{y} = {x}");
+//});
+
+//app.Run(HandleRequst);
+//async Task HandleRequst(HttpContext context)
+//{
+//    await context.Response.WriteAsync("Hello METANIT.COM 2");
+//}
+//app.UseWelcomePage();
+//app.MapGet("/",
+//    (HttpContext context) =>
+//    {
+//        return "Hello World!";
+//    });
 
 
 //var acceptLanguages = context.Request.GetTypedHeaders().AcceptLanguage.OrderByDescending(x => x.Quality ?? 1);
