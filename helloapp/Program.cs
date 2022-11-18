ba863f17caf48d4b2d9a1661b46a90a477c609a6
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-18 17:06:35 +0300

    redirecting

--- helloapp/Program.cs
+++ helloapp/Program.cs
@@ -6,32 +6,48 @@ WebApplication app = builder.Build();
 
 app.Run(async (context) =>
 {
-    context.Response.ContentType = "text/html; charset=utf-8";
-
-    // если обращение идет по адресу "/postuser", получаем данные формы
-    if (context.Request.Path == "/postuser")
+    if (context.Request.Path == "/old")
     {
-        var form = context.Request.Form;
-        string name = form["name"];
-        string age = form["age"];
-        string[] languages = form["languages"];
-        // создаем из массива languages одну строку
-        string langList = "";
-        foreach (var lang in languages)
-        {
-            langList += $" {lang}";
-        }
-        await context.Response.WriteAsync($"<div><p>Name: {name}</p>" +
-            $"<p>Age: {age}</p>" +
-            $"<div>Languages:{langList}</ul></div>");
+        context.Response.Redirect("/new");
+    }
+    else if (context.Request.Path == "/old2")
+    {
+        context.Response.Redirect("https://www.google.com/search?q=metanit.com");
+    }
+    else if (context.Request.Path == "/new")
+    {
+        await context.Response.WriteAsync("New Page");
     }
     else
     {
-        await context.Response.SendFileAsync("html/index.html");
+        await context.Response.WriteAsync("Main Page");
     }
 });
 
 app.Run();
+
+//context.Response.ContentType = "text/html; charset=utf-8";
+//// если обращение идет по адресу "/postuser", получаем данные формы
+//if (context.Request.Path == "/postuser")
+//{
+//    var form = context.Request.Form;
+//    string name = form["name"];
+//    string age = form["age"];
+//    string[] languages = form["languages"];
+//    // создаем из массива languages одну строку
+//    string langList = "";
+//    foreach (var lang in languages)
+//    {
+//        langList += $" {lang}";
+//    }
+//    await context.Response.WriteAsync($"<div><p>Name: {name}</p>" +
+//        $"<p>Age: {age}</p>" +
+//        $"<div>Languages:{langList}</ul></div>");
+//}
+//else
+//{
+//    await context.Response.SendFileAsync("html/index.html");
+//}
 
 //PhysicalFileProvider fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
 //IFileInfo fileinfo = fileProvider.GetFileInfo("forest.jpg");
