1d7842c06eb588c6fa670a4d3d671afb67b76069
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-12 23:55:25 +0300

    RenameCollectionAsync DropCollectionAsync

--- MongoApp/MongoApp/Program.cs
+++ MongoApp/MongoApp/Program.cs
@@ -11,7 +11,9 @@ namespace MongoApp
                 MongoClient client = new MongoClient("mongodb://localhost:27017");
                 IMongoDatabase database = client.GetDatabase("test");
                 var db = client.GetDatabase("test");
-               // await db.CreateCollectionAsync("people");  // создаем коллекцию "people"
+                // await db.CreateCollectionAsync("people");  // создаем коллекцию "people"
+                //await db.RenameCollectionAsync("people", "users"); // из people в users
+                await db.DropCollectionAsync("users");  // удаляем коллекцию "users"
 
                 var collections = await db.ListCollectionNamesAsync(); // await db.ListCollectionsAsync();
                 foreach (var collection in collections.ToList())
@@ -21,6 +23,7 @@ namespace MongoApp
             }
             catch (Exception ex)
             {
+                Type t = ex.GetType();
                 Console.WriteLine(ex);
             }
 
