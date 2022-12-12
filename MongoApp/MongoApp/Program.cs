cfd6d320543390a523111b87fef28e3b10e88e25
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-12 23:42:56 +0300

    mongodb console client

---
+++ MongoApp/MongoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+
+namespace MongoApp
+{
+    internal class Program
+    {
+        static async Task Main() // string[] args
+        {
+            try
+            {
+                MongoClient client = new MongoClient("mongodb://localhost:27017");
+                using (var cursor = await client.ListDatabasesAsync())
+                {
+                    var databases = cursor.ToList();
+                    foreach (var database in databases)
+                    {
+                        Console.WriteLine(database);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
