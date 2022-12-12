5060fe2f3e493a68ba846875dd10b26ab63d8af9
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-13 01:13:25 +0300

    var employees = await collection.Aggregate().Group

--- MongoApp/MongoApp/Program.cs
+++ MongoApp/MongoApp/Program.cs
@@ -3,6 +3,7 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
 using System;
+using System.Reflection.Metadata;
 using System.Text.Json;
 
 namespace MongoApp
@@ -15,11 +16,18 @@ namespace MongoApp
             {
                 MongoClient client = new MongoClient("mongodb://localhost:27017");
                 var db = client.GetDatabase("test");
-                var collection = db.GetCollection<BsonDocument>("users");
+                var collection = db.GetCollection<BsonDocument>("employees");
 
-                var users = await collection.Find("{}").Project("{Name:1, Age:1, _id:0}").ToListAsync();
+                var employees = await collection.Aggregate()
+                    .Group(new BsonDocument {
+        { "_id", "$Company.Title" },               // группируем по имени компании
+        { "count", new BsonDocument("$sum", 1) }, // получаем количество документов в группе
+                    })
+                    .ToListAsync();
 
-                foreach (var user in users) Console.WriteLine(user);
+                foreach (var employeeGroup in employees)
+                    Console.WriteLine(employeeGroup);
+
             }
             catch (Exception ex)
             {
@@ -30,4 +38,6 @@ namespace MongoApp
             Console.WriteLine();
         }
     }
+
+    record Person2(string? Name);
 }
