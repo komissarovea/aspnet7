71334d8ccfa69da164445aec3133dd909a81f981
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-12 23:49:02 +0300

    GetDatabase DropDatabaseAsync

--- MongoApp/MongoApp/Program.cs
+++ MongoApp/MongoApp/Program.cs
@@ -9,14 +9,21 @@ namespace MongoApp
             try
             {
                 MongoClient client = new MongoClient("mongodb://localhost:27017");
-                using (var cursor = await client.ListDatabasesAsync())
-                {
-                    var databases = cursor.ToList();
-                    foreach (var database in databases)
-                    {
-                        Console.WriteLine(database);
-                    }
-                }
+                //using (var cursor = await client.ListDatabasesAsync())
+                //using (var cursor = await client.ListDatabaseNamesAsync())
+                //{
+                //    var databases = cursor.ToList();
+                //    foreach (var database in databases)
+                //    {
+                //        Console.WriteLine(database);
+                //    }
+                //}
+
+                IMongoDatabase database = client.GetDatabase("test");
+                var grades = database.GetCollection<object>("grades");
+                Console.WriteLine(grades);
+
+                await client.DropDatabaseAsync("test2");
             }
             catch (Exception ex)
             {
