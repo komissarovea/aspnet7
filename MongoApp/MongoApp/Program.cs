e47cb2bb97d1a0ceddec09dca62630ae6631f62d
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-13 00:45:48 +0300

    var builder = Builders<BsonDocument>.Filter;  var filter = builder.Eq("Name", "Tom");

--- MongoApp/MongoApp/Program.cs
+++ MongoApp/MongoApp/Program.cs
@@ -15,14 +15,15 @@ namespace MongoApp
             {
                 MongoClient client = new MongoClient("mongodb://localhost:27017");
                 var db = client.GetDatabase("test");
-                // получаем из бд коллекцию users
-                // var collection = db.GetCollection<Person>("users");
 
                 var collection = db.GetCollection<BsonDocument>("users");
+                // определяем построитель фильтров
+                var builder = Builders<BsonDocument>.Filter;
 
-                var filter = new BsonDocument("Name", new BsonDocument { { "$regex", "m$" } });
+                // определяем фильтр - находим все документы, где Name = "Tom"
+                var filter = builder.Eq("Name", "Tom");
 
-                List<BsonDocument> users = await collection.Find(filter).ToListAsync();
+                var users = await collection.Find(filter).ToListAsync();
                 foreach (var user in users)
                 {
                     Console.WriteLine(user);
