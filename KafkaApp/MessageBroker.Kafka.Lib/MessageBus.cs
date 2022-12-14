5f0e13764a71743e4dedd7913bdb83ac515e4c63
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-14 17:59:00 +0300

    Kafka sample

---
+++ KafkaApp/MessageBroker.Kafka.Lib/MessageBus.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+// using Confluent.Kafka.Serialization;
+
+namespace MessageBroker.Kafka.Lib
+{
+    public sealed class MessageBus : IDisposable
+    {
+        private readonly IProducer<Null, string> _producer;
+
+        private readonly IDictionary<string, string> _producerConfig;
+        private readonly IDictionary<string, string> _consumerConfig;
+
+        public MessageBus() : this("localhost") { }
+
+        public MessageBus(string host)
+        {
+            _producerConfig = new Dictionary<string, string> { { "bootstrap.servers", host } };
+            _consumerConfig = new Dictionary<string, string>
+            {
+                { "group.id", "custom-group"},
+                { "bootstrap.servers", host }
+            };
+
+            _producer = new ProducerBuilder<Null, string>(_producerConfig.ToList()).Build();
+                //new Producer<Null, string>(_producerConfig, null, new StringSerializer(Encoding.UTF8));
+        }
+
+        public void SendMessage(string topic, string message)
+        {
+            _producer.ProduceAsync(topic, new Message<Null, string>() { Value = message });
+        }
+
+        public void SubscribeOnTopic<T>(string topic, Action<T> action, CancellationToken cancellationToken) where T : class
+        {
+            using (var consumer = new ConsumerBuilder<Null, string>(_consumerConfig.ToList()).Build())
+            //new Consumer<Null, string>(_consumerConfig, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.Assign(new List<TopicPartitionOffset> { new TopicPartitionOffset(topic, 0, -1) });
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var result = consumer.Consume();
+                    if (result != null)
+                    {
+                        action((result.Message.Value as T)!);
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _producer?.Dispose();
+        }
+    }
+}
---
+++ KafkaApp/NameService/Program.cs
@@ -0,0 +1,88 @@
+﻿using MessageBroker.Kafka.Lib;
+
+namespace NameService
+{
+    internal class Program
+    {
+        private static readonly string userHelpMsg = "NameService.\nEnter 'b' or 'g' to process boy or girl names respectively";
+        private static readonly string bTopicNameCmd = "b_name_command";
+        private static readonly string gTopicNameCmd = "g_name_command";
+
+        private static readonly string bTopicNameResp = "b_name_response";
+        private static readonly string gTopicNameResp = "g_name_response";
+        private static readonly string[] _boyNames =
+        {
+            "Arsenii",
+            "Igor",
+            "Kostya",
+            "Ivan",
+            "Dmitrii",
+        };
+        private static readonly string[] _girlNames =
+        {
+            "Nastya",
+            "Lena",
+            "Ksusha",
+            "Katya",
+            "Olga"
+        };
+
+        static void Main(string[] args)
+        {
+            bool canceled = false;
+
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                canceled = true;
+            };
+
+            using (var msgBus = new MessageBus())
+            {
+                Console.WriteLine(userHelpMsg);
+
+                HandleUserInput(Console.ReadLine()!, msgBus);
+
+                while (!canceled) { }
+            }
+        }
+
+        private static void HandleUserInput(string userInput, MessageBus msgBus)
+        {
+            switch (userInput)
+            {
+                case "b":
+                    Task.Run(() => msgBus.SubscribeOnTopic<string>(bTopicNameCmd, (msg) => BoyNameCommandListener(msg, msgBus), CancellationToken.None));
+                    Console.WriteLine("Processing boy names");
+                    break;
+                case "g":
+                    Task gTask = Task.Run(() => msgBus.SubscribeOnTopic<string>(gTopicNameCmd, (msg) => GirlNameCommandListener(msg, msgBus), CancellationToken.None));
+                    Console.WriteLine("Processing girl names");
+                    break;
+                default:
+                    Console.WriteLine($"Unknown command. {userHelpMsg}");
+                    HandleUserInput(Console.ReadLine()!, msgBus);
+                    break;
+            }
+        }
+
+        private static void BoyNameCommandListener(string msg, MessageBus msgBus)
+        {
+            Console.WriteLine(msg);
+            var r = new Random().Next(0, 5);
+            var randName = _boyNames[r];
+
+            msgBus.SendMessage(bTopicNameResp, randName);
+            Console.WriteLine($"Sending {randName}");
+        }
+
+        private static void GirlNameCommandListener(string msg, MessageBus msgBus)
+        {
+            var r = new Random().Next(0, 5);
+            var randName = _girlNames[r];
+
+            msgBus.SendMessage(gTopicNameResp, randName);
+            Console.WriteLine($"Sending {randName}");
+        }
+    }
+}
