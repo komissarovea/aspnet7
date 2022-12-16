1d7916a4277ca8dfef5829a05b6efbfcf745ce0c
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-16 18:54:16 +0300

    post form 2

--- MvcApp/MvcApp/Controllers/HomeController.cs
+++ MvcApp/MvcApp/Controllers/HomeController.cs
@@ -9,25 +9,50 @@ namespace MvcApp.Controllers
 
     public class HomeController : ControllerBase
     {
-        [HttpGet]
         public async Task Index()
         {
-            string content = @"<form method='post'>
-                <label>Name:</label><br />
-                <input name='name' /><br />
-                <label>Age:</label><br />
-                <input type='number' name='age' /><br />
-                <input type='submit' value='Send' />
-            </form>";
+            string form = @"
+<form method='post'>
+   <p><input name='[0]' /></p>
+    <p><input name='[2]' /></p>
+    <p><input name='[1]' /></p>
+    <input type='submit' value='Send' />
+</form>
+";
             Response.ContentType = "text/html;charset=utf-8";
-            await Response.WriteAsync(content);
+            await Response.WriteAsync(form);
+        }
+        [HttpPost]
+        public string Index(string[] names)
+        {
+            string result = "";
+            foreach (string name in names)
+            {
+                result = $"{result} {name}";
+            }
+            return result;
         }
 
-        [HttpPost]
-        public string Index(Person person) => $"{person.Name}: {person.Age}";
+
+        //[HttpGet]
+        //public async Task Index()
+        //{
+        //    string content = @"<form method='post'>
+        //        <label>Name:</label><br />
+        //        <input name='name' /><br />
+        //        <label>Age:</label><br />
+        //        <input type='number' name='age' /><br />
+        //        <input type='submit' value='Send' />
+        //    </form>";
+        //    Response.ContentType = "text/html;charset=utf-8";
+        //    await Response.WriteAsync(content);
+        //}
 
         //[HttpPost]
-        //public string Index(string name, int age) => $"{name}: {age}";
+        //public string Index(Person person) => $"{person.Name}: {person.Age}";
+
+        ////[HttpPost]
+        ////public string Index(string name, int age) => $"{name}: {age}";
     }
 
 }
