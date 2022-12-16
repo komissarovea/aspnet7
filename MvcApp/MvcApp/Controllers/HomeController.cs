5b907a2e3eaeb9822834f844046ac0d022c68741
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-16 15:05:39 +0300

    ControllerContext.HttpContext await Response.WriteAsync

--- MvcApp/MvcApp/Controllers/HomeController.cs
+++ MvcApp/MvcApp/Controllers/HomeController.cs
@@ -8,22 +8,34 @@ namespace MvcApp.Controllers
     //[NonController]
     public class HomeController : ControllerBase
     {
-        //[NonAction]
-        [HttpPost]
-        public string Index()
+        public async Task Index()
         {
-            return "Hello METANIT.COM";
+            var ctx1 = ControllerContext.HttpContext;
+            var ctx2 = HttpContext;
+            bool b = ctx1 == ctx2;
+            b = ctx2.Request == Request;
+            b = ctx2.Response == Response;
+
+            Response.ContentType = "text/html;charset=utf-8";
+            //await Response.WriteAsync("<h2>Hello METANIT.COM</h2>");
+            System.Text.StringBuilder tableBuilder = new("<h2>Request headers</h2><table>");
+            foreach (var header in Request.Headers)
+            {
+                tableBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+            }
+            tableBuilder.Append("</table>");
+            await Response.WriteAsync(tableBuilder.ToString());
         }
 
-        [ActionName("Index")]
-        [HttpHead, HttpOptions, HttpGet, HttpPost, HttpPatch, HttpPut,  HttpDelete]
+        //[ActionName("Index")]
+        [HttpHead, HttpOptions, HttpPost, HttpPatch, HttpPut,  HttpDelete]
         public string Hello()
         {
             return "Hello ASP.NET";
         }
 
         [HttpPost, HttpOptions]
-        public string Hello2() => null;
+        public string Hello2() => "Hello 22";
 
         protected internal string Hello3() => "Hello 333";
 
