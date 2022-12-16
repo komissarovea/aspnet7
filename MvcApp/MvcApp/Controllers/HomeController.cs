4e5cc20ba66ec45dd2e84cb5c522241588212634
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-16 18:49:22 +0300

    post form 1

--- MvcApp/MvcApp/Controllers/HomeController.cs
+++ MvcApp/MvcApp/Controllers/HomeController.cs
@@ -9,36 +9,25 @@ namespace MvcApp.Controllers
 
     public class HomeController : ControllerBase
     {
-        // https://localhost:7288/Home/Index?name=Tom&age=37
-        public string Index()
+        [HttpGet]
+        public async Task Index()
         {
-            string name = Request.Query["name"];
-            string age = Request.Query["age"];
-            return $"Name: {name}  Age: {age}";
+            string content = @"<form method='post'>
+                <label>Name:</label><br />
+                <input name='name' /><br />
+                <label>Age:</label><br />
+                <input type='number' name='age' /><br />
+                <input type='submit' value='Send' />
+            </form>";
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync(content);
         }
 
-        //// https://localhost:7288/Home/Index?people=Tom&people=Bob&people=Sam
-        //// https://localhost:7288/Home/Index?people[0]=Tom&people[2]=Bob&people[1]=Sam
-        //// https://localhost:7288/Home/Index?[0]=Tom&[2]=Bob&[1]=Sam
-        //public string Index(string[] people)
-        //{
-        //    string result = "";
-        //    foreach (var person in people)
-        //        result = $"{result}{person}; ";
-        //    return result;
-        //}
+        [HttpPost]
+        public string Index(Person person) => $"{person.Name}: {person.Age}";
 
-        //// https://localhost:7288/Home/Index?name=Tom&age=37
-        //public string Index(Person person)
-        //{
-        //    return $"Person Name: {person.Name}  Person Age: {person.Age}";
-        //}
-
-        //public string Index(string name = "Bob", int age = 33)
-        //{
-        //    return $"Name: {name}  Age: {age}";
-        //}
-
-        //public string Index(string name) => $"Your name: {name}";       
+        //[HttpPost]
+        //public string Index(string name, int age) => $"{name}: {age}";
     }
+
 }
