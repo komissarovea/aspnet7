9edd244deb3883372383333f3cbdb0984b9e3346
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-16 20:09:40 +0300

    JsonSerializerOptions

--- MvcApp/MvcApp/Controllers/HomeController.cs
+++ MvcApp/MvcApp/Controllers/HomeController.cs
@@ -7,20 +7,30 @@ namespace MvcApp.Controllers
 {
     public record class Person(string Name, int Age);
 
-    public class HomeController : ControllerBase
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            Person tom = new Person("Tom", 37);
+
+            var jsonOptions = new System.Text.Json.JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true, // учитываем регистр
+                WriteIndented = true                // отступы для красоты
+            };
+            return Json(tom, jsonOptions);
+
+            //return Content("Hello METANIT.COM");
 
             //return new ObjectResult(new Person("Tom", 37));
-            return new NoContentResult();
+            //return new NoContentResult();
             //return new ContentResult() { Content = "<h2>Hello METANIT.COM!</h2>" };
         }
 
-        public ActionResult GetVoid1()
-        {
-            return new EmptyResult();
-        }
+        //public JsonResult GetName()
+        //{
+        //    return Json("Tom");
+        //}
     }
 
 }
