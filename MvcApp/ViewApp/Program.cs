c9c5c2cd0e17d5cf7cff85d228596286ed858d2d
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-17 00:26:52 +0300

    AddControllersWithViews

---
+++ MvcApp/ViewApp/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// добавляем поддержку контроллеров с представлениями
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+// устанавливаем сопоставление маршрутов с контроллерами
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
