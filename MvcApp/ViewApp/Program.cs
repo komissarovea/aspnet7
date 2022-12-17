a0d6ff3d021e0c0e7055c089d63c63e37d93b2bc
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-17 13:15:50 +0300

    @inject ITimeService timeService

--- MvcApp/ViewApp/Program.cs
+++ MvcApp/ViewApp/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // добавл€ем поддержку контроллеров с представлени€ми
 builder.Services.AddControllersWithViews();
+// внедр€ем сервис ITimeService
+builder.Services.AddTransient<ITimeService, SimpleTimeService>();
 var app = builder.Build();
 
 // устанавливаем сопоставление маршрутов с контроллерами
@@ -10,3 +12,12 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+public interface ITimeService
+{
+    string Time { get; }
+}
+public class SimpleTimeService : ITimeService
+{
+    public string Time => DateTime.Now.ToShortTimeString();
+}
