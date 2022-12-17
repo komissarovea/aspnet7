a259e677540906723d02adf0e49e382ab50adda8
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-17 13:20:52 +0300

    form 1

--- MvcApp/ViewApp/Controllers/HomeController.cs
+++ MvcApp/ViewApp/Controllers/HomeController.cs
@@ -4,7 +4,15 @@ namespace ViewApp.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index() => View();
+
+        [HttpPost]
+        public string Index(string username, string password, int age, string comment)
+        {
+            return $"User Name: {username}   Password: {password}   Age: {age}  Comment: {comment}";
+        }
+
         public IActionResult About() => View();
 
         public IActionResult Hello()
